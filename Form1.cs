65700eeb3a3e6fe9f5b544573343d34fabe7da01
Act3Sun <act3jo@gmail.com>
2025-03-12 20:09:51 +0900

    ✨ OSCでVRCのチャットにメッセージの送信機能を実装

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Threading;
+using System.Runtime;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using Rug.Osc;
+using Rug.Osc.Core;
 namespace HelloVRChat_Csharp;
 
 public partial class Form1 : Form
 {
+    private OscSender OscS ;
     public Form1()
     {
         InitializeComponent();
+        SendMsg();
+    }
+    private void SendMsg ()
+    {
+        // ホスト名を取得
+        string hostName = Dns.GetHostName();
+
+        // ホスト名からIPアドレスを取得
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            int PortNo = 9000;
+
+            OscS = new OscSender(address,0,PortNo);
+            OscS.Connect();
+            OscS.Send(new OscMessage("/chatbox/input","Hello, world!"));
+    }
+   private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        // アプリケーションが終了する前にOSCセンダーを閉じる
+        OscS.Close();
     }
 }
